b8d8a2787821af30fcff33a8638fffd7dc28bfa8
Makoena20 <koenaletseku@gmail.com>
2024-01-16 01:39:33 -0800

    1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import random; number = random.randint(-10000, 10000); print("Last digit of", number, "is", number % 10, "and is", "greater than 5" if number % 10 > 5 else "0" if number % 10 == 0 else "less than 6 and not 0")
+
