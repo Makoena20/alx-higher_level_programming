db407683ccee9442e4164758c61a250368bad19f
Makoena20 <koenaletseku@gmail.com>
2024-01-16 04:37:10 -0800

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,5 @@
+print(magic_calculation(1, 2, 3))   # Should return 3
+print(magic_calculation(2, 1, 3))   # Should return 3
+print(magic_calculation(3, 1, 2))   # Should return 3
+print(magic_calculation(30, 10, 2)) # Should return 300
+
