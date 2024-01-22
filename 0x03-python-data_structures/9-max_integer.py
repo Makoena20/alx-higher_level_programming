10d2b3888fbe1c5647d53b7f07c5042b116c3d6b
Makoena20 <koenaletseku@gmail.com>
2024-01-21 23:07:18 -0800

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    Finds the maximum integer in a list.
+
+    Parameters:
+    - my_list (list): The list of integers.
+
+    Returns:
+    - The maximum integer in the list.
+    - None if the list is empty.
+    """
+    if not my_list:
+        return None
+
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
+
+if __name__ == "__main__":
+    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    max_value = max_integer(my_list)
+    print("Max: {}".format(max_value))
+
