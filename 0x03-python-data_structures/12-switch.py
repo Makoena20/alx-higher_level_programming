96f0f833c09b2c4ecd10ecd1fbe9a6348fc01270
Makoena20 <koenaletseku@gmail.com>
2024-01-21 23:56:54 -0800

    12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-Switch the values of a and b
-"""
-
 a = 89
 b = 10
-# Your code goes here (line 4)
+# Your code goes here
 a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
 
