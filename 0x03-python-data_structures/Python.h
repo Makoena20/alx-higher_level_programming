108b8cd7269d75a8eed2e19bbbf08c21fcc81baf
Makoena20 <koenaletseku@gmail.com>
2024-01-22 05:01:11 -0800

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/Python.h
@@ -0,0 +1,11 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+/* This is a placeholder for the necessary includes and declarations from Python's headers.
+   Please make sure to include the required headers and declarations here. */
+
+#include <Python.h>
+
+#endif /* PYTHON_H */
+
+
