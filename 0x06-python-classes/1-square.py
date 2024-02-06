98f0aba90b8dfa9bc034ece14b1e1323b3ac3827
Makoena20 <koenaletseku@gmail.com>
2024-02-05 22:23:30 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
+
