53ffda41f8a3129dd227b91e1266132947966074
Makoena20 <koenaletseku@gmail.com>
2024-02-08 00:19:48 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-import sys
-
-sys.stdout.write(
-    "object" + "oriented" + " programming" + " with" + " Python" + "\n"
-)
+print("object" + "-" + "oriented" + " " + "programming" + " " + "with" + " " + "Python\n".replace("-", " "))
