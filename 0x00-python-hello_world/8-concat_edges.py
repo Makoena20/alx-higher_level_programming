d78a4d5f418f314ffd45d13ce947ece711aa9002
Makoena20 <koenaletseku@gmail.com>
2024-02-08 00:30:55 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print("object" + "-" + "oriented" + " " + "programming" + " " + "with" + " " + "Python\n".replace("-", " "))
+str_1 = "object"
+str_2 = "oriented"
+str_3 = "programming"
+str_4 = "with"
+str_5 = "Python\n"
+print(str_1 + "-" + str_2 + " " + str_3 + " " + str_4 + " " + str_5.replace("-", " "))
