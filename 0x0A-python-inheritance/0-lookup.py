7106f3f77a507e4bf21cf4b3c4c27e210166d126
Makoena20 <koenaletseku@gmail.com>
2024-02-20 01:53:54 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,7 +2,13 @@
 
 def lookup(obj):
     """
-    Returns the list of available attributes and methods of an object
+    Returns the list of available attributes and methods of an object.
+    
+    Args:
+        obj: An object whose attributes and methods need to be looked up.
+        
+    Returns:
+        A list containing the attributes and methods of the object.
     """
     return dir(obj)
 
