732a4195e1b13a0fb8ab09a5b3c9d6314934443f
Makoena20 <koenaletseku@gmail.com>
2024-02-20 23:23:31 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+Module for inherits_from method
+"""
+
+
 def inherits_from(obj, a_class):
     """
-    Function that returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class;
-    otherwise False.
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
