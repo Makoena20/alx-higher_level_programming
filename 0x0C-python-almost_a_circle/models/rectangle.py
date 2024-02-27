cb516a33830f903bbc59e215d0656e63ac2034f4
Makoena20 <koenaletseku@gmail.com>
2024-02-27 05:27:13 -0800

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -786,3 +786,91 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+
+"""Defines a Rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width of the Rectangle."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height of the Rectangle."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set x coordinate of the Rectangle."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set y coordinate of the Rectangle."""
+        self.__y = value
+
+    def area(self):
+        """Compute the area of the Rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """Display the Rectangle."""
+        print('\n' * self.y, end='')
+        for i in range(self.height):
+            print(' ' * self.x, end='')
+            print('#' * self.width)
+
+    def __str__(self):
+        """Return the string representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle."""
+        attrs = ["id", "width", "height", "x", "y"]
+        if args:
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Rectangle."""
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
