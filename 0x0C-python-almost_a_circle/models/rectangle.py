ee9f43a78d99a3fc1078b24eb476213804a66109
Makoena20 <koenaletseku@gmail.com>
2024-02-27 21:52:37 -0800

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1062,3 +1062,59 @@ class Rectangle:
 
 if __name__ == "__main__":
     pass
+
+
+
+""" Rectangle Module """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Class Rectangle that inherits from Base """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize Rectangle instance """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for y """
+        self.__y = value
