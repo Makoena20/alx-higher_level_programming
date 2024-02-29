10e7a85cdec16c46d4e3fb685d45d6e918f6eef3
Makoena20 <koenaletseku@gmail.com>
2024-02-29 00:30:43 -0800

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""
+Module for saving Python objects to a text file using JSON representation.
+"""
+
+
+import json
+
 
 def save_to_json_file(my_obj, filename):
     """
-    Save an object to a JSON file.
+    Save an object to a text file in JSON format.
 
     Args:
-        my_obj: Object to be saved.
-        filename: Name of the file to save the object to.
-    """
-    import json
+        my_obj (any): Python object to be serialized.
+        filename (str): Name of the file to save the JSON representation.
 
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+    Returns:
+        None
+    """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
