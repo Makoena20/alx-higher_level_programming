f384a1bbf5e33e03f79702d3fad3365d79eacc47
Makoena20 <koenaletseku@gmail.com>
2024-02-29 21:36:19 -0800

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
+"""Class to JSON module"""
+
+
 def class_to_json(obj):
-    """Converts a class instance to a JSON serializable dictionary"""
-    json_dict = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (int, str, bool)):
-            json_dict[key] = value
-        elif isinstance(value, list):
-            json_dict[key] = value
-        elif isinstance(value, dict):
-            json_dict[key] = value
-    return json_dict
+    """
+    Function that returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
