02ab7ee3414f536cc73f348630b8fba2509b2c35
Makoena20 <koenaletseku@gmail.com>
2024-03-27 06:27:39 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,7 +1,6 @@
-/*
-    This script lists all rows of the table first_table from the database hbtn_0c_0.
-*/
+-- Script to list all rows of the table first_table from the database hbtn_0c_0
 
--- Select all fields from the table first_table
+-- Display all fields from first_table
 SELECT * FROM first_table;
 
+-- Correct output: table without record
