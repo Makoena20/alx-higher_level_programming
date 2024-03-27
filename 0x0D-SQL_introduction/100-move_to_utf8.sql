ee1809b3a736685c635ae40409637f431d359076
Makoena20 <koenaletseku@gmail.com>
2024-03-27 06:52:08 -0700

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+/*
+Script to convert the hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+*/
+
+/* Correct conversion */
+
+/* Convert database to UTF8 */
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+/* Convert table to UTF8 */
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+/* Convert field to UTF8 */
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
