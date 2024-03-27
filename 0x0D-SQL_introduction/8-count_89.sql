31dbca56c21ab72acce7db67152bf8504d91bd7f
Makoena20 <koenaletseku@gmail.com>
2024-03-27 06:32:30 -0700

    8-count_89.sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+/*
+   This SQL script counts the number of records with id = 89 in the first_table of the hbtn_0c_0 database.
+*/
+
+-- Count records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
