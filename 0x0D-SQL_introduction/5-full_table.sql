6af935e4ac7f4341ead85bc2b891256bbefb5dc2
Makoena20 <koenaletseku@gmail.com>
2024-03-27 06:22:07 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,11 +1,6 @@
-/*
- * This script prints the full description of the table first_table from the database hbtn_0c_0
- * without using the DESCRIBE or EXPLAIN statements.
- */
+-- Print the full description of the table first_table from the database hbtn_0c_0 without using DESCRIBE or EXPLAIN statements
 
-/* Retrieve table structure using INFORMATION_SCHEMA */
-
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_COMMENT
+/* Retrieve table information */
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
-
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
