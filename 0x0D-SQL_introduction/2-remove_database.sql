9540863c1fb29a98642230d5048174342a1b1d67
Makoena20 <koenaletseku@gmail.com>
2024-03-27 03:53:59 -0700

    2-remove_database.sql

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,8 @@
+-- Script to delete the database hbtn_0c_0 if it exists
+
+-- Check if the database hbtn_0c_0 exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
+-- Create the database hbtn_0c_0
+CREATE DATABASE hbtn_0c_0;
+
