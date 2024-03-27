d96d940f1ba42d2f95593901510b29f25d9b91ee
Makoena20 <koenaletseku@gmail.com>
2024-03-27 01:40:46 -0700

    0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,7 +1,12 @@
-/*
- * 0-list_databases.sql
- * Script to list all databases on the MySQL server
- */
+-- Script to list all databases on the MySQL server
 
+-- List initial databases sorted
 SHOW DATABASES;
 
+-- Create multiple databases for testing
+CREATE DATABASE test_db1;
+CREATE DATABASE test_db2;
+CREATE DATABASE test_db3;
+
+-- List databases after creation sorted
+SHOW DATABASES;
