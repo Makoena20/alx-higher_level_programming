038570a26145bbecfcd0657a72b89b903ec01067
Makoena20 <koenaletseku@gmail.com>
2024-03-27 23:42:44 -0700

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,13 +1,8 @@
-/* 
-   Task:
-   List all the cities of California that can be found in the database hbtn_0d_usa.
-   The states table contains only one record where name = California (but the id can be different, as per the example)
-   Results must be sorted in ascending order by cities.id
-   You are not allowed to use the JOIN keyword
-*/
+-- Task: Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California (but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
 
--- Selecting cities of California using a subquery
 SELECT * FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
-
