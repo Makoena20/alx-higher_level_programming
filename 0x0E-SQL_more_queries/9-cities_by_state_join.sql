edb4ee0c22e77b886a1b617de0184e5413a2b487
Makoena20 <koenaletseku@gmail.com>
2024-03-27 23:45:35 -0700

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,12 +1,10 @@
-/*
-Script to list all cities contained in the database hbtn_0d_usa
-Each record should display: cities.id - cities.name - states.name
-Results must be sorted in ascending order by cities.id
-You can use only one SELECT statement
+/* 
+   List all cities contained in the database hbtn_0d_usa.
+   Each record should display: cities.id - cities.name - states.name
+   Results must be sorted in ascending order by cities.id
 */
 
 SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
-
