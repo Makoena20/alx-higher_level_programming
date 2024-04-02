bfa804ec10f1282a99bdd95657dc45a1d2d08ac6
Makoena20 <koenaletseku@gmail.com>
2024-04-02 05:04:56 -0700

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,16 +1,7 @@
-/*
-  Task:
-  Write a script that lists all cities contained in the database hbtn_0d_usa.
-
-  Each record should display: cities.id - cities.name - states.name
-  Results must be sorted in ascending order by cities.id
-  You can use only one SELECT statement
-
-  Requirements:
-  - All SQL keywords should be in uppercase
-  - All your SQL queries should have a comment just before
-  - All your files should start by a comment describing the task
-*/
+-- Task: Write a script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
 
 SELECT cities.id, cities.name, states.name
 FROM cities
