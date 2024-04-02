e25ce8098e8f4ab539da778e1141725cf36c0537
Makoena20 <koenaletseku@gmail.com>
2024-04-02 05:11:30 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,13 +1,7 @@
-/*
-List all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-Each record should display: tv_shows.title - tv_show_genres.genre_id
-Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-You can use only one SELECT statement
-*/
+/* Script to list all shows in hbtn_0d_tvshows with at least one linked genre */
 
--- SELECT statement to list shows with at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
