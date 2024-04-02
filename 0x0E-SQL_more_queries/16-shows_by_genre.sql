307ed39041c701993d3614e8f20e3abacdc92342
Makoena20 <koenaletseku@gmail.com>
2024-04-02 06:32:09 -0700

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,10 @@
 /*
-Script to list all shows and their linked genres from the hbtn_0d_tvshows database.
+Script to list all shows and their linked genres from the hbtn_0d_tvshows database
+If a show doesn’t have a genre, display NULL in the genre column
+Results sorted in ascending order by show title and genre name
 */
 
 SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_shows.id = tv_genres.show_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
-
