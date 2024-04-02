9c2d7d7cf919ac8ba28fd66c063941c9ef262e4b
Makoena20 <koenaletseku@gmail.com>
2024-04-02 06:26:14 -0700

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,14 +1,18 @@
 /*
-  Task: Write a script that lists all shows and their associated genres from the database hbtn_0d_tvshows.
-  If a show doesn’t have a genre, display NULL in the genre column.
-  Each record should display: tv_shows.title - tv_genres.name
-  Results must be sorted in ascending order by the show title and genre name.
+    Task: Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+    
+    If a show doesn’t have a genre, display NULL in the genre column.
+    
+    Each record should display: tv_shows.title - tv_genres.name.
+    
+    Results must be sorted in ascending order by the show title and genre name.
+    
+    You can use only one SELECT statement.
 */
 
--- List all shows and their associated genres
 SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS name
 FROM tv_shows
-LEFT JOIN show_genres ON tv_shows.id = show_genres.show_id
-LEFT JOIN tv_genres ON show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, name ASC;
 
