2df14c5711512d2cce46dcb7a53693d95766aed2
Makoena20 <koenaletseku@gmail.com>
2024-04-21 05:47:27 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/main_0_0.sql
+++ 0x0E-SQL_more_queries/main_0_0.sql
@@ -1,17 +1,6 @@
--- Corrected Output: id_not_null doesn’t exist and regular inserts
--- No SELECT or SHOW statements allowed
+/*
+Main script to execute 103-rating_genres.sql
+*/
 
--- Script to create id_not_null table if not exists
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    name VARCHAR(256)
-);
-
--- Script to insert data into id_not_null table
-INSERT INTO id_not_null (id, name) VALUES (1, 'Holberton School');
-INSERT INTO id_not_null (id, name) VALUES (1, 'Python is cool');
-INSERT INTO id_not_null (id, name) VALUES (2, 'Holberton');
-INSERT INTO id_not_null (id, name) VALUES (3, 'School');
-INSERT INTO id_not_null (id, name) VALUES (4, 'C is fun');
-INSERT INTO id_not_null (id, name) VALUES (89, 'Best School');
+cat 103-rating_genres.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
 
