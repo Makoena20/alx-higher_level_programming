07600df50610b44eaa1e495dc5ffed5eaf7466ef
Makoena20 <koenaletseku@gmail.com>
2024-04-21 05:35:21 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,12 +1,13 @@
--- List all genres in the database hbtn_0d_tvshows_rate by their rating.
--- Each record should display: tv_genres.name - rating sum
--- Results must be sorted in descending order by their rating
--- You can use only one SELECT statement
+/*
+Script to list all genres in the hbtn_0d_tvshows_rate database by their rating
+Each record displays: tv_genres.name - rating sum
+Results are sorted in descending order by rating
+*/
 
-SELECT TV_GENRES.name AS name, SUM(RATING) AS rating
-FROM TV_SHOWS
-JOIN TV_SHOWS_GENRES ON TV_SHOWS.id = TV_SHOWS_GENRES.tv_show_id
-JOIN TV_GENRES ON TV_GENRES.id = TV_SHOWS_GENRES.tv_genre_id
-GROUP BY TV_GENRES.name
+SELECT tv_genres.name, SUM(rating) AS rating
+FROM tv_genres
+JOIN tv_shows ON tv_genres.id = tv_shows.genre_id
+JOIN ratings ON tv_shows.id = ratings.show_id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
 
