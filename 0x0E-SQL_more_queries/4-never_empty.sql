0d2b81ee7f3fd1bb800a733ebe22544214cf0873
Makoena20 <koenaletseku@gmail.com>
2024-04-21 04:58:55 -0700

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,22 +1,16 @@
-/*
- * Task: Create a script that creates the table id_not_null on the MySQL server.
- *       The table should have an 'id' column of type INT with a default value of 1,
- *       and a 'name' column of type VARCHAR(256).
- *       If the table id_not_null already exists, the script should not fail.
- */
-
--- Create table id_not_null if it doesn't exist
+-- Check if id_not_null table exists
 CREATE TABLE IF NOT EXISTS id_not_null (
-    -- id column with default value 1
     id INT DEFAULT 1,
-    -- name column
     name VARCHAR(256)
 );
 
--- Insert values into the table id_not_null
--- Regular inserts
-INSERT INTO id_not_null (id, name) VALUES (89, "Best School");
+-- Insert values
+INSERT INTO id_not_null (id, name) VALUES (1, "Holberton School");
+INSERT INTO id_not_null (id, name) VALUES (1, "Python is cool");
+INSERT INTO id_not_null (id, name) VALUES (2, "Holberton");
+INSERT INTO id_not_null (id, name) VALUES (3, "School");
+INSERT INTO id_not_null (id, name) VALUES (4, "C is fun");
 
--- Insert values without specifying id
-INSERT INTO id_not_null (name) VALUES ("Best");
+-- Check final table content
+SELECT * FROM id_not_null;
 
