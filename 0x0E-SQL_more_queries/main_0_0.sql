cea7b37d4f0c9200eccaf824363d470cd1f6ace5
Makoena20 <koenaletseku@gmail.com>
2024-04-21 05:01:47 -0700

    4-never_empty.sql

--- 0x0E-SQL_more_queries/main_0_0.sql
+++ 0x0E-SQL_more_queries/main_0_0.sql
@@ -1,3 +1,6 @@
+-- Corrected Output: id_not_null doesn’t exist and regular inserts
+-- No SELECT or SHOW statements allowed
+
 -- Script to create id_not_null table if not exists
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
@@ -10,4 +13,5 @@ INSERT INTO id_not_null (id, name) VALUES (1, 'Python is cool');
 INSERT INTO id_not_null (id, name) VALUES (2, 'Holberton');
 INSERT INTO id_not_null (id, name) VALUES (3, 'School');
 INSERT INTO id_not_null (id, name) VALUES (4, 'C is fun');
+INSERT INTO id_not_null (id, name) VALUES (89, 'Best School');
 
