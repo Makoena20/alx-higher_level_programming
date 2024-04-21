479f97922cd816f83400d14a425b6b49d56886c2
Makoena20 <koenaletseku@gmail.com>
2024-04-21 05:38:26 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,13 +1,12 @@
 /*
-Script to list all genres in the hbtn_0d_tvshows_rate database by their rating
-Each record displays: tv_genres.name - rating sum
-Results are sorted in descending order by rating
+Script to list all genres by their rating
+Only one SELECT statement allowed
+Results sorted in descending order by rating
 */
 
-SELECT tv_genres.name, SUM(rating) AS rating
+SELECT tv_genres.name, SUM(tv_genres.rating) AS rating
 FROM tv_genres
-JOIN tv_shows ON tv_genres.id = tv_shows.genre_id
-JOIN ratings ON tv_shows.id = ratings.show_id
+JOIN tv_shows ON tv_genres.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
 
