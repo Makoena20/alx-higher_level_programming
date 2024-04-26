8f99b917c01b4a3cf65def8dfd48533789b1817a
Makoena20 <koenaletseku@gmail.com>
2024-04-26 01:25:27 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""
+Script that lists all states with a name starting with N (upper N) from the
+database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Execute SQL query
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-
-    # Fetch all rows using fetchall() method
-    rows = cursor.fetchall()
-
-    # Print results
-    for row in rows:
-        print(row)
-
-    # Close the cursor
-    cursor.close()
-
-    # Close the database connection
-    db.close()
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
 
