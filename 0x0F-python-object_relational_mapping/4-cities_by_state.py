5c208ab6446329af848faac8ec05b2236de57f26
Makoena20 <koenaletseku@gmail.com>
2024-04-27 06:33:44 -0700

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=database)
+        cursor = db.cursor()
+        cursor.execute("""SELECT cities.id, cities.name, states.name
+                          FROM cities
+                          JOIN states ON cities.state_id = states.id
+                          ORDER BY cities.id""")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        exit(1)
+
+    exit(0)
+
