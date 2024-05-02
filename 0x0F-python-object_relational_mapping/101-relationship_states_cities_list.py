d56f773336c46c549f52dfe5f43fa846bb48fce6
Makoena20 <koenaletseku@gmail.com>
2024-05-02 05:23:44 -0700

    101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python3
-"""Module that lists all State objects and corresponding City objects in a database"""
+"""Script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa"""
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
+from relationship_state import Base, State, City
 
-def list_states_cities(username, password, db_name):
-    """Lists all State objects and corresponding City objects in the database"""
+if __name__ == "__main__":
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
-    # Create engine to connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, db_name), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, password, db_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    # Create session maker bound to engine
     Session = sessionmaker(bind=engine)
-
-    # Create session
     session = Session()
 
-    # Query to fetch all states and corresponding cities
     states = session.query(State).order_by(State.id).all()
 
-    # Iterate over states and print corresponding cities
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    list_states_cities(sys.argv[1], sys.argv[2], sys.argv[3])
-
+    session.close()
