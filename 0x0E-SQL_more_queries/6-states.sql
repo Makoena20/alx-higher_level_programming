6b8c6e6148096879193ed7be444c62109f15cc94
Makoena20 <koenaletseku@gmail.com>
2024-05-03 02:44:12 -0700

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,18 +1,18 @@
--- Task: Create a script to create the hbtn_0d_usa database and the states table, insert data into it, and handle existing database and table scenarios.
-
--- Create or use the database hbtn_0d_usa if it exists
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
 USE hbtn_0d_usa;
 
--- Create the states table if it doesn't exist
+-- Create table if it doesn't exist
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
 
 -- Insert data into the states table
-INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas');
-
--- Attempt to select data from the states table
--- SELECT * FROM states; -- You are not allowed to use the SELECT statement in the script
+INSERT INTO states (name) VALUES 
+    ('California'), 
+    ('Arizona'), 
+    ('Texas');
 
