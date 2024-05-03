cf824333e8f31bf843c371fed0e618c0c42db733
Makoena20 <koenaletseku@gmail.com>
2024-05-03 02:51:01 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,10 @@
--- List all shows with at least one genre linked
+-- This script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record displays: tv_shows.title - tv_show_genres.genre_id
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- Only one SELECT statement is used
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
