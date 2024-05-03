c9fc64cfb19025b82bed5c61125b70212a76ea90
Makoena20 <koenaletseku@gmail.com>
2024-05-03 02:41:38 -0700

    5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,13 +1,6 @@
--- Create or ensure existence of table unique_id with specified schema
+-- Create the unique_id table if it doesn't exist
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
-
--- Insertion of data into unique_id table
--- Correct output: unique_id doesn’t exist and regular inserts
--- Correct output: unique_id already exists, regular inserts and insert with id already inserted
-
-INSERT INTO unique_id (id, name) VALUES (89, "Best School");
-INSERT INTO unique_id (id, name) VALUES (89, "Best");
 
