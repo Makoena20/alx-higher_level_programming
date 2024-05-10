5cffb38c77edc01f4814b52899b7a3abc884b5e9
Makoena20 <koenaletseku@gmail.com>
2024-05-10 03:14:30 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status
+Module to fetch and display information from a URL.
 """
 
 import urllib.request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
+
     with urllib.request.urlopen(url) as response:
-        body = response.read()
+        data = response.read()
 
     print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content:", data.decode('utf-8'))
 
