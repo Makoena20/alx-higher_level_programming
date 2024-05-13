e500b4582fc04c31d156937e430932400c4cb3c4
Makoena20 <koenaletseku@gmail.com>
2024-05-13 03:33:41 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
 
