7d96e95d7b656ebfdde4d2fe66989c18d2560837
Makoena20 <koenaletseku@gmail.com>
2024-06-04 05:21:41 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,39 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
   if (error) {
-    console.error(error);
-    return;
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+  } else {
+    const filmData = JSON.parse(body);
+    const charactersUrls = filmData.characters;
+    const charactersPromises = charactersUrls.map(url => new Promise((resolve, reject) => {
+      request(url, function (error, response, body) {
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(`Invalid status code: ${response.statusCode}`);
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        }
+      });
+    }));
+
+    Promise.all(charactersPromises)
+      .then(characters => {
+        characters.forEach(character => console.log(character));
+      })
+      .catch(error => {
+        console.error('Error fetching character data:', error);
+      });
   }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
 });
 
